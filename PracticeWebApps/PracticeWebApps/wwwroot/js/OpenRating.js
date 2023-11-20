d57a50736e42abec45996dd8c73e20fa45d4b538
nazim117 <61702781+nazim117@users.noreply.github.com>
2023-11-20 01:07:29 +0100

    added rating front-end

---
+++ PracticeWebApps/PracticeWebApps/wwwroot/js/OpenRating.js
@@ -0,0 +1,5 @@
+﻿document.getElementById('showRatingButton').addEventListener('click', function () {
+    var ratingSection = document.getElementById('ratingSection');
+    ratingSection.style.display =
+        (ratingSection.style.display == 'none' || ratingSection.style.display == '') ? 'block' : 'none';
+});
